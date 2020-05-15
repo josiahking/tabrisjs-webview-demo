2ce3e48c42c1df3e4ac3fabe524beb54e276646e
Josiah King <josiahaccounts@gmail.com>
2020-05-15 12:49:37 +0100

    added toast plugin sh: q: command not found

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 const {WebView, contentView, app} = require('tabris');
 
+let lastBackClickTime = 0;
+
 let webView = new WebView({
   left: 0, right: 0, top: 0, bottom: 0,
   url: 'http://jobman.uk.tempcloudsite.com'
@@ -7,7 +9,15 @@ let webView = new WebView({
 }).appendTo(contentView);
 
 app.onBackNavigation((e) => {
-	e.preventDefault();
-	console.log('back fired');
-	webView.goBack();
+	let now = Date.now();
+	if((now - lastBackClickTime) >= 900 && (now - lastBackClickTime) <= 1200){
+		e.preventDefault();
+		window.plugins.toast.showLongBottom('Press back again to exit');
+		lastBackClickTime = now;
+	}
+	if ((now - lastBackClickTime) >= 1210){
+		e.preventDefault();
+		webView.goBack();
+		lastBackClickTime = now;
+	}
 });
